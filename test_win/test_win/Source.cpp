31417b78d6371f6b2c3d209a5a813bad93713cab
czy0538 <czy0538@gmail.com>
2019-05-14 20:16:49 +0800

    绪论1

--- test_win/test_win/Source.cpp
+++ test_win/test_win/Source.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 int main()
 {
-	cout << "hello world" << endl;
+	srand(time(0));
+	int i = rand() % 30;
+	while (i == 9||i==4||i==17)
+	{
+		i = rand() % 30;
+	}
+	cout << i << endl;
 	return 0;
 }
---
+++ 绪论/绪论/1-01A-5有穷性Hailstone(n).cpp
@@ -0,0 +1,25 @@
+//Hailstone(n)
+#include<iostream>
+using namespace std;
+using LL=long long;
+void hailstone(LL n)
+{
+	int length = 1;
+	while (1 < n)
+	{
+		if (n % 2 == 0)
+			n /= 2;
+		else
+			n = 3 * n + 1;
+		++length;
+		cout << n << endl;
+	}
+	cout <<"ÔËËã´ÎÊý"<< length << endl;
+}
+int main()
+{
+	LL n;
+	cin >> n;
+	hailstone(n);
+	return 0;
+}
