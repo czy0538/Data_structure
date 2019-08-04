832df04724eea064e77d27a920f2aac0274d899c
czy0538 <czy0538@gmail.com>
2019-08-04 16:18:28 +0800

    2019年8月4日 16:18:24

--- 浙江大学/week1/函数运行时间测量模版.cpp
+++ 浙江大学/week1/函数运行时间测量模版.cpp
@@ -2,18 +2,17 @@
 #include<ctime>
 //我的时间测量函数
 
-	clock_t t_start, t_stop;
-	double duration;
-	void start()
-	{
-		t_start = clock();
-	}
-	void display()
-	{
-		std::cout << "Runtime:" << ((double)(t_stop - t_start)) / CLK_TCK << std::endl;
-	}
-	void stop()
-	{
-		t_stop = clock();
-		display();
-	}
+long long t_start, t_stop;
+void start()
+{
+	t_start = clock();
+}
+void display()
+{
+	std::cout << "Runtime:" << (double)(t_stop - t_start) / CLK_TCK << std::endl;
+}
+void stop()
+{
+	t_stop = clock();
+	display();
+}
--- 浙江大学/week2/List_Array/ListArray.cpp
+++ 浙江大学/week2/List_Array/ListArray.cpp
@@ -1,48 +1,64 @@
+//#include<iostream>
+//constexpr long long MAXSIZE = 10;
+//template<typename T>
+//class List
+//{
+//private:
+//	T Data[MAXSIZE];
+//	long long Last;
+//public:
+//	List()
+//	{
+//		Last = -1;
+//	}
+//	long long Find(T x)
+//	{
+//		long long i = 0;
+//		while (i <= Last && Data[i] != x)
+//			++i;
+//		if (i > Last)
+//			return -1;
+//		else return i;
+//	}
+//	long long Insert(T x,long long i)
+//	{
+//		
+//		if (Last == MAXSIZE - 1)
+//		{
+//			cout << "线性表已经满了" << endl;
+//			return -1;
+//		}
+//		if (i < 1 || i < Last + 2)
+//		{
+//			cout << "非法位置" << endl;
+//			return -1'
+//		}
+//		for (int j = Last; j >= i - 1; j--)
+//		{
+//			Data[j + 1] = Data[j];
+//		}
+//		Data[i - 1] = x;
+//		++Last;
+//		return i - 1;
+//	}
+//	long long Delete(long long i)
+//	{
+//		if(i<1||)
+//	}
+//};
 #include<iostream>
-constexpr long long MAXSIZE = 10;
-template<typename T>
-class List
+using namespace std;
+int main()
 {
-private:
-	T Data[MAXSIZE];
-	long long Last;
-public:
-	List()
+	int array[4];
+	int j = 0;
+	for (auto& i : array)
 	{
-		Last = -1;
+		i = j;
+		++j;
 	}
-	long long Find(T x)
+	for (auto i : array)
 	{
-		long long i = 0;
-		while (i <= Last && Data[i] != x)
-			++i;
-		if (i > Last)
-			return -1;
-		else return i;
+		cout << i << endl;
 	}
-	long long Insert(T x,long long i)
-	{
-		
-		if (Last == MAXSIZE - 1)
-		{
-			cout << "线性表已经满了" << endl;
-			return -1;
-		}
-		if (i < 1 || i < Last + 2)
-		{
-			cout << "非法位置" << endl;
-			return -1'
-		}
-		for (int j = Last; j >= i - 1; j--)
-		{
-			Data[j + 1] = Data[j];
-		}
-		Data[i - 1] = x;
-		++Last;
-		return i - 1;
-	}
-	long long Delete(long long i)
-	{
-		if(i<1||)
-	}
-};
+}
